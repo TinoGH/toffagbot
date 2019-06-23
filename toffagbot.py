7ba5e2c39768f143bd4abb52e17d33d8d6a8967f
tino <etienne.lomet8@gmail.com>
2019-06-23 12:14:16 +0200

    improved memory (robots instances) from global to client class property

--- toffagbot.py
+++ toffagbot.py
@@ -4,10 +4,17 @@ from Letters import Letters
 
 TOKEN = os.environ['TOKEN']
 
-robots = {}
-
 
 class MyClient(discord.Client):
+
+    def __init__(self):
+        discord.Client.__init__(self)
+        self.robots = {}
+
+    def read(self, author, text):
+        if author not in self.robots.keys():
+            self.robots[author] = Letters(6)
+        self.robots[author].read(text + "\n")
 
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
@@ -15,33 +22,27 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         if message.author == client.user:
             return
-
-        global robots
         
         if message.content.startswith("toffagbot write"):
-            if message.author not in robots.keys():
+            if message.author not in self.robots.keys():
                 await message.channel.send("Blup bloup... :/")
             else:
-                text = robots[message.author].write(100)
+                text = self.robots[message.author].write(100)
                 if len(text) > 0:
-                    await message.channel.send(robots[message.author].write(100))
+                    await message.channel.send(text)
                 else:
                     await message.channel.send("Blup bloup... :/")
 
         elif message.content.startswith("toffagbot learn"):
             async for message in message.channel.history(limit=10000):
-                if message.author not in robots.keys():
-                    robots[message.author] = Letters(6)
-                robots[message.author].read(message.content + "\n")
+                self.read(message.author, message.content)
             await message.channel.send("blip bloup blip... blipblip!")
 
         elif message.content.startswith("toffagbot"):
             await message.channel.send("Blop? Oo")
 
         else:
-            if message.author not in robots.keys():
-                robots[message.author] = Letters(6)
-            robots[message.author].read(message.content + "\n")
+            self.read(message.author, message.content)
                 
                 
 client = MyClient()
